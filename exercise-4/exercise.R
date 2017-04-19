03dac4a46fed4a96f535bef0a3ad9b8364229642
Siddarth nalegave <Siddarth nalegave>
2017-04-19 16:31:35 -0700

    finised few exercises

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,25 +1,30 @@
 # Exercise 4: DPLYR and flights data
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
+View(flights)
+dim(flights)
+nrow(flights)
+ncol(flights)
+?flights
 
-
-# Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+# Add a column that is the amount of time gained in the air
+flights <- mutate(flights, gain = arr_delay - dep_delay)
 
 # Sort your data.frame desceding by the column you just created
+flights <- arrange(flights, desc(gain))
 
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- flights %>% mutate(time.gained = arr_delay - dep_delay) %>% arrange(desc(gain))
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$gain)
 
 # On average, did flights gain or lose time?
 
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -5,17 +5,27 @@
 library(nycflights13)
 library(dplyr)
 
+View(flights)
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
+
+flights <- group_by(flights, month)
+dep.delay.by.month <- summarize(flights, delay = mean(dep_delay, na.rm=TRUE))
+
+# dep.delay.by.month <- flights %>% 
+#   group_by(month) %>%  
+#   summarise(delay = mean(dep_delay, na.rm=TRUE))
+
 
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
-
+plot(dep.delay.by.month)
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
-
+flights <- group_by(flights, dest)
+airport.with.highest.avg.delay <- summarize(flights, delay = mean(arr_delay, na.rm=TRUE))
+airport.with.highest.avg.delay <- arrange(airport.with.highest.avg.delay, -delay)
 ### Bonus ###
 # Which city was flown to with the highest average speed?
